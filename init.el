f4480b9fbbb67983b89d073cf227a22ebd1ee4be
Andrea Alberti <a.alberti82@gmail.com>
2025-02-23 23:06:18 +0100

    Configure emacsclient as default editor; added alias e for emacsclient

--- init.el
+++ init.el
@@ -24,3 +24,18 @@
 ;; Customize the mode line font
 (set-face-attribute 'mode-line nil :font "MesloLGS NF" :height 120 :weight 'bold)
 (set-face-attribute 'mode-line-inactive nil :font "MesloLGS NF" :height 120)
+
+;; Set syntax for ssh config file
+; (add-to-list 'auto-mode-alist '("/.ssh/config" . ssh-config-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(ssh-config-mode catppuccin-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
