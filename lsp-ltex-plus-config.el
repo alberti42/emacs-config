cfa05d8ec82196e860a2544bf049a22f8124ba20
Andrea Alberti <a.alberti82@gmail.com>
2026-02-16 01:19:05 +0100

    Fixed autostart of lsp-ltex-plus

--- lsp-ltex-plus-config.el
+++ lsp-ltex-plus-config.el
@@ -5,7 +5,7 @@
 ;; Use the intended Emacs client `lsp-ltex-plus` and an externally installed
 ;; `ltex-ls-plus` server (provided on PATH, e.g. via zinit).
 ;;
-;; Scope: enable only in Markdown and TeX modes.
+;; Scope: enable only in Markdown and TeX-ish modes.
 ;;
 
 ;;; Code:
@@ -15,22 +15,43 @@
              :type git
              :host github
              :repo "emacs-languagetool/lsp-ltex-plus")
-  :after lsp-mode
+  ;; Don't delay hook installation: with `:after lsp-mode` the hooks would only
+  ;; be added once lsp-mode is loaded, which defeats auto-start.
+  :defer t
   :init
+  ;; Limit the client to the modes we care about.
+  ;; NOTE: `lsp-ltex-plus` ships with a broader default list (e.g. text-mode,
+  ;; org-mode, rst-mode, etc.).
+  (setq lsp-ltex-plus-active-modes
+        '(markdown-mode gfm-mode
+          latex-mode LaTeX-mode
+          tex-mode plain-tex-mode TeX-mode))
+
+  ;; Ensure TeX modes use a language id that LTEX+ enables/parses as LaTeX.
+  ;; (LTEX+ defaults to identifiers like "latex"/"markdown"; there is no
+  ;; standard "plaintex" identifier.)
+  (with-eval-after-load 'lsp-mode
+    (dolist (pair '((tex-mode . "latex")
+                    (plain-tex-mode . "latex")
+                    (TeX-mode . "latex")))
+      (add-to-list 'lsp-language-id-configuration pair)))
+
+  (defun my--lsp-ltex-plus-enable ()
+    "Enable LTEX+ in the current buffer." 
+    (require 'lsp-ltex-plus)
+    (lsp-deferred))
+
   (setq lsp-ltex-plus-language "en-US")
   (setq lsp-ltex-plus-check-frequency "edit")
   ;; Make diagnostics visible.
   (setq lsp-ltex-plus-diagnostic-severity "warning")
-  ;; Enable LTEX+ checks for the language IDs used by lsp-mode.
-  ;; - markdown-mode/gfm-mode => "markdown"
-  ;; - latex-mode/LaTeX-mode  => "latex"
-  ;; - plain-tex-mode         => "plaintex"
-  (setq lsp-ltex-plus-enabled ["markdown" "latex" "plaintex"])
+  ;; Enable LTEX+ checks for the language IDs we use.
+  (setq lsp-ltex-plus-enabled ["markdown" "latex"])
   :hook
-  ((markdown-mode gfm-mode plain-tex-mode latex-mode LaTeX-mode) .
-   (lambda ()
-     (require 'lsp-ltex-plus)
-     (lsp-deferred))))
+  ((markdown-mode gfm-mode
+                  tex-mode plain-tex-mode TeX-mode
+                  latex-mode LaTeX-mode) .
+   my--lsp-ltex-plus-enable))
 
 (provide 'lsp-ltex-plus-config)
 
