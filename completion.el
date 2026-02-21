8264ebcf3c3f153fc0f1b9c8ea8a8a0e01ffabc1
Andrea Alberti <a.alberti82@gmail.com>
2026-02-21 11:30:35 +0100

    emacs: make `M-x' context-aware by default

--- completion.el
+++ completion.el
@@ -8,6 +8,20 @@
 
 ;;; Code:
 
+;; Make `M-x' context-aware by default.
+;;
+;; Emacs can filter the set of commands shown by `execute-extended-command'
+;; based on whether they apply in the current context (major mode, minibuffer,
+;; active completion UIs, etc.). This improves signal-to-noise in `M-x' and also
+;; hides internal/transient commands from packages like Corfu, which are not
+;; meant to be called directly.
+;;
+;; If you ever miss a command in `M-x', this is the knob to revisit.
+(use-package emacs
+  :straight nil
+  :init
+  (setq read-extended-command-predicate #'command-completion-default-include-p))
+
 (use-package icomplete
   :straight nil
   :init
