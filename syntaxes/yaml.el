a22b1127c5bb5ede97dbc4c73a25db5f697db0b7
Andrea Alberti <a.alberti82@gmail.com>
2026-03-08 13:00:54 +0100

    Added LSP support for YAML (syntax highlighting)

---
+++ syntaxes/yaml.el
@@ -0,0 +1,15 @@
+;;; syntaxes/yaml.el --- YAML syntax highlighting -*- lexical-binding: t; -*-
+
+(defvar emacs-config-syntaxes-enable-yaml t
+  "Whether to enable YAML settings from syntaxes/yaml.el.")
+
+(when emacs-config-syntaxes-enable-yaml
+  (use-package yaml-mode
+    :mode ("\\.ya?ml\\'" . yaml-mode)
+    :hook (yaml-mode . (lambda ()
+                         (setq-local indent-tabs-mode nil)
+                         (setq-local yaml-indent-offset 2)))))
+
+(provide 'syntaxes-yaml)
+
+;;; syntaxes/yaml.el ends here
