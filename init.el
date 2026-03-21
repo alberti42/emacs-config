5178aef44b7e041521e05e96e5ea2411b77aeccf
Andrea Alberti <a.alberti82@gmail.com>
2026-03-21 23:08:11 +0100

    Disabled mac-pseudo-daemon-config

--- init.el
+++ init.el
@@ -49,9 +49,9 @@
 ;; macOS pseudo-daemon
 ;; Keep Dock icon + menu functional after closing the last GUI frame when using
 ;; emacs in server/daemon style workflows.
-(emacs-config-load-module
-  'mac-pseudo-daemon-config
-  "Could not load mac-pseudo-daemon-config.el; macOS pseudo-daemon behavior is disabled.")
+;; (emacs-config-load-module
+;;   'mac-pseudo-daemon-config
+;;   "Could not load mac-pseudo-daemon-config.el; macOS pseudo-daemon behavior is disabled.")
 
 ;; Save minibuffer history
 (savehist-mode 1)
