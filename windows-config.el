21435ae37770b3d40a2129a9a8dabaf5c6fb6b50
Andrea Alberti <a.alberti82@gmail.com>
2026-03-22 14:13:02 +0100

    Auto-detect tmux from emacs and emacsclient

--- windows-config.el
+++ windows-config.el
@@ -2,45 +2,57 @@
 
 ;;; Code:
 
+(defun windows-config--in-tmux-p ()
+  "Return non-nil if the current frame is running inside a tmux session.
+In daemon mode, uses the frame's `environment' parameter which reflects the
+connecting client's environment. Falls back to `getenv' for non-daemon Emacs."
+  (let ((env (frame-parameter nil 'environment)))
+    (if env
+        (cl-some (lambda (s) (string-prefix-p "TMUX=" s)) env)
+      (and (getenv "TMUX") t))))
+
 (defun windmove-left-or-tmux ()
   "Move focus to the window to the left, or the tmux pane to the left if at the edge."
   (interactive)
   (if (window-in-direction 'left)
       (windmove-left)
-    (shell-command "tmux if -F '#{pane_at_left}' '' 'select-pane -L'")))
+    (when (windows-config--in-tmux-p)
+      (call-process-shell-command "tmux if -F '#{pane_at_left}' '' 'select-pane -L'" nil nil))))
+
 
 (defun windmove-right-or-tmux ()
   "Move focus to the window to the right, or the tmux pane to the right if at the edge."
   (interactive)
   (if (window-in-direction 'right)
       (windmove-right)
-    (shell-command "tmux if -F '#{pane_at_right}' '' 'select-pane -R'")))
+    (when (windows-config--in-tmux-p)
+      (call-process-shell-command "tmux if -F '#{pane_at_right}' '' 'select-pane -R'" nil nil))))
 
 (defun windmove-up-or-tmux ()
   "Move focus to the window above, or the tmux pane above if at the edge."
   (interactive)
   (if (window-in-direction 'above)
       (windmove-up)
-    (shell-command "tmux if -F '#{pane_at_top}' '' 'select-pane -U'")))
+    (when (windows-config--in-tmux-p)
+      (call-process-shell-command "tmux if -F '#{pane_at_top}' '' 'select-pane -U'" nil nil))))
 
 (defun windmove-down-or-tmux ()
   "Move focus to the window below, or the tmux pane below if at the edge."
   (interactive)
   (if (window-in-direction 'below)
       (windmove-down)
-    (shell-command "tmux if -F '#{pane_at_bottom}' '' 'select-pane -D'")))
+    (when (windows-config--in-tmux-p)
+      (call-process-shell-command "tmux if -F '#{pane_at_bottom}' '' 'select-pane -D'" nil nil))))
 
 ;; windmove: navigate between windows with C-c <arrow>.
-;; When running inside tmux, bind to the -or-tmux variants so navigation
-;; falls through to tmux pane switching at the Emacs window edge.
+;; Always bind to the -or-tmux variants; they check the terminal environment
+;; at call time so the tmux fallback only fires when actually inside tmux.
 (use-package windmove
   :straight nil
-  :config
-  (let ((in-tmux (getenv "TMUX")))
-    (global-set-key (kbd "C-c <left>")  (if in-tmux #'windmove-left-or-tmux  #'windmove-left))
-    (global-set-key (kbd "C-c <right>") (if in-tmux #'windmove-right-or-tmux #'windmove-right))
-    (global-set-key (kbd "C-c <up>")    (if in-tmux #'windmove-up-or-tmux    #'windmove-up))
-    (global-set-key (kbd "C-c <down>")  (if in-tmux #'windmove-down-or-tmux  #'windmove-down))))
+  :bind (("C-c <left>"  . windmove-left-or-tmux)
+         ("C-c <right>" . windmove-right-or-tmux)
+         ("C-c <up>"    . windmove-up-or-tmux)
+         ("C-c <down>"  . windmove-down-or-tmux)))
 
 ;; Window resizing: grow the current window in the given direction.
 ;; Uses repeat-mode: after the initial C-c C-<arrow>, keep pressing C-<arrow>
