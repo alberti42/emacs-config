31a8da222d3f48c25ef86ac2642f46685eed398e
Andrea Alberti <a.alberti82@gmail.com>
2026-03-22 22:45:09 +0100

    Use shell-file-name when starting term instead of showing the prompt

--- terminal-config.el
+++ terminal-config.el
@@ -5,6 +5,21 @@
 (setq term-term-name "xterm-256color")
 (setq eshell-term-name "xterm-256color")
 
+;; Initialize ESHELL to shell-file-name (which in turn is initialized to $SHELL)
+(setenv "ESHELL" shell-file-name)
+
+;; Override term to skip the prompt, using shell-file-name
+;; Lazy-loading of term
+(with-eval-after-load 'term
+  (fset 'term--original (symbol-function 'term))
+  (defun term (program)
+    (interactive (list shell-file-name))
+    (term--original program))
+
+  ;; Close the window (and kill the buffer) when the terminal process exits
+  (advice-add 'term-handle-exit :after
+    (lambda (&rest _) (quit-window t))))
+
 ;; vterm: fast, accurate terminal emulator backed by libvterm.
 (use-package vterm
   :straight t)
