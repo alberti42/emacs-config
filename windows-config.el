e132b52a25635fb3a7d56b2483658a8d8519c2c4
Andrea Alberti <a.alberti82@gmail.com>
2026-03-22 13:37:15 +0100

    Added tmux support to windows navigation functions

--- windows-config.el
+++ windows-config.el
@@ -2,13 +2,45 @@
 
 ;;; Code:
 
+(defun windmove-left-or-tmux ()
+  "Move focus to the window to the left, or the tmux pane to the left if at the edge."
+  (interactive)
+  (if (window-in-direction 'left)
+      (windmove-left)
+    (shell-command "tmux if -F '#{pane_at_left}' '' 'select-pane -L'")))
+
+(defun windmove-right-or-tmux ()
+  "Move focus to the window to the right, or the tmux pane to the right if at the edge."
+  (interactive)
+  (if (window-in-direction 'right)
+      (windmove-right)
+    (shell-command "tmux if -F '#{pane_at_right}' '' 'select-pane -R'")))
+
+(defun windmove-up-or-tmux ()
+  "Move focus to the window above, or the tmux pane above if at the edge."
+  (interactive)
+  (if (window-in-direction 'above)
+      (windmove-up)
+    (shell-command "tmux if -F '#{pane_at_top}' '' 'select-pane -U'")))
+
+(defun windmove-down-or-tmux ()
+  "Move focus to the window below, or the tmux pane below if at the edge."
+  (interactive)
+  (if (window-in-direction 'below)
+      (windmove-down)
+    (shell-command "tmux if -F '#{pane_at_bottom}' '' 'select-pane -D'")))
+
 ;; windmove: navigate between windows with C-c <arrow>.
+;; When running inside tmux, bind to the -or-tmux variants so navigation
+;; falls through to tmux pane switching at the Emacs window edge.
 (use-package windmove
   :straight nil
-  :bind (("C-c <left>"  . windmove-left)
-         ("C-c <right>" . windmove-right)
-         ("C-c <up>"    . windmove-up)
-         ("C-c <down>"  . windmove-down)))
+  :config
+  (let ((in-tmux (getenv "TMUX")))
+    (global-set-key (kbd "C-c <left>")  (if in-tmux #'windmove-left-or-tmux  #'windmove-left))
+    (global-set-key (kbd "C-c <right>") (if in-tmux #'windmove-right-or-tmux #'windmove-right))
+    (global-set-key (kbd "C-c <up>")    (if in-tmux #'windmove-up-or-tmux    #'windmove-up))
+    (global-set-key (kbd "C-c <down>")  (if in-tmux #'windmove-down-or-tmux  #'windmove-down))))
 
 ;; Window resizing: grow the current window in the given direction.
 (bind-keys ("C-c C-<right>" . enlarge-window-horizontally)
