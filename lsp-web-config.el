021e7932560034a13fd19a8f5529d833f5f8be2c
Andrea Alberti <a.alberti82@gmail.com>
2026-04-03 16:56:45 +0200

    Fixed support for TypeScript

--- lsp-web-config.el
+++ lsp-web-config.el
@@ -5,15 +5,18 @@
 ;; JS/TS LSP via typescript-language-server.
 ;; Install the server with: npm install -g typescript-language-server typescript
 ;;
+;; Note: lsp-mode is the engine; `lsp-javascript` is the built-in client.
 
 ;;; Code:
 
-(use-package typescript-mode
-  :hook (typescript-mode . lsp-deferred))
+;; lsp-mode itself is configured in lsp-core.el.
+;; This module only adds Web-specific activation.
 
-(use-package js
+(use-package lsp-javascript
   :straight nil
-  :hook (js-mode . lsp-deferred))
+  :after lsp-mode
+  :hook ((typescript-mode . lsp-deferred)
+         (js-mode . lsp-deferred)))
 
 (provide 'lsp-web-config)
 
