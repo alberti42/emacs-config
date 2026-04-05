7b98b9f80ae9d9efda32dac0f86d0f11ba96d514
Andrea Alberti <a.alberti82@gmail.com>
2026-04-05 21:43:24 +0200

    Disabled inheritenv

--- early-init.el
+++ early-init.el
@@ -13,9 +13,8 @@
 ;; so PATH is correct from the very start.  Uses the same file-truename
 ;; trick as init.el to work through the ~/.config/emacs symlink.
 ;; Skipped for daemon: the launcher already set up the environment.
-(unless (daemonp)
-  (let ((dir (file-name-directory (file-truename (or load-file-name buffer-file-name)))))
-    (load (expand-file-name "env-config" dir) nil 'nomessage)))
+(let ((dir (file-name-directory (file-truename (or load-file-name buffer-file-name)))))
+  (load (expand-file-name "env-config" dir) nil 'nomessage))
 
 ;; Default font for GUI frames (TTY frames ignore font face attributes).
 (set-face-attribute 'default nil :family "JetBrainsMonoNL Nerd Font Mono" :height 180 :weight 'light)
