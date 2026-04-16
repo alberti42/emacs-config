78f0bef6b9b7439c86e2ebc6acf5dbde60424360
Andrea Alberti <a.alberti82@gmail.com>
2026-04-16 17:59:12 +0200

    Disabled debug

--- lsp-ltex-plus-config.el
+++ lsp-ltex-plus-config.el
@@ -21,7 +21,7 @@
   (lsp-ltex-plus-lt-server-uri "https://api.languagetoolplus.com")
   (lsp-ltex-plus-lt-server-uri nil)
   (lsp-ltex-plus-diagnostic-severity "warning")
-  (lsp-ltex-plus-debug t)
+  (lsp-ltex-plus-debug nil)
   (lsp-ltex-plus-trace-server "message")
   
   (lsp-ltex-plus-apply-kind-first-patch t)
