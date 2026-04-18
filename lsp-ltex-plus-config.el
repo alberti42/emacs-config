5a1163ed71752fc225f29bb4805d9448c5d462fc
Andrea Alberti <a.alberti82@gmail.com>
2026-04-18 16:06:07 +0200

    Explicitly set language

--- lsp-ltex-plus-config.el
+++ lsp-ltex-plus-config.el
@@ -18,6 +18,7 @@
 
 
   :custom
+  (lsp-ltex-plus-language "en-US")
   (lsp-ltex-plus-lt-server-uri "https://api.languagetoolplus.com")
   (lsp-ltex-plus-lt-server-uri nil)
   (lsp-ltex-plus-diagnostic-severity "warning")
