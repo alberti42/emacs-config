2599bf5098b43de3a63b9acef37352c72d96dc84
Andrea Alberti <a.alberti82@gmail.com>
2026-04-25 21:08:39 +0200

    Added customization of jupyter-repl-mode

---
+++ syntaxes/jupyter-repl.el
@@ -0,0 +1,13 @@
+;;; syntaxes/jupyter-repl.el --- jupyter-repl-mode display settings -*- lexical-binding: t; -*-
+
+(defvar emacs-config-syntaxes-enable-jupyter-repl t
+  "Whether to enable jupyter-repl settings from syntaxes/jupyter-repl.el.")
+
+(when emacs-config-syntaxes-enable-jupyter-repl
+  (add-hook 'jupyter-repl-mode-hook
+            (lambda ()
+              (display-line-numbers-mode -1))))
+
+(provide 'syntaxes-jupyter-repl)
+
+;;; syntaxes/jupyter-repl.el ends here
