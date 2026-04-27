815de039c435d545c1a638e6ad8719f7449c4b68
Andrea Alberti <a.alberti82@gmail.com>
2026-04-27 21:56:19 +0200

    Added variants for code-cells eval + advance

--- code-cells-config.el
+++ code-cells-config.el
@@ -9,13 +9,23 @@
 (use-package code-cells
   :straight t
   :hook ((python-mode python-ts-mode) . code-cells-mode-maybe)
-  :config
+  :config  
+  (defun my/code-cells-eval-maybe-advance (&optional advance)
+    "Evaluate the current cell.
+With a prefix argument ADVANCE (\\[universal-argument]), move point to
+the next cell after evaluating."
+    (interactive "P")
+    (call-interactively #'code-cells-eval)
+    (when advance
+      (code-cells-forward-cell)
+      (recenter-top-bottom 0)))
+
   ;; A set of shorter keybindings recommended by the package author
   (let ((map code-cells-mode-map))
     (keymap-set map "M-p" 'code-cells-backward-cell)
     (keymap-set map "M-n" 'code-cells-forward-cell)
     (keymap-set map "M-RET" 'jupyter-eval-line-or-region)
-    (keymap-set map "C-c C-c" 'code-cells-eval)
+    (keymap-set map "C-c C-c" 'my/code-cells-eval-maybe-advance)
     ;; When emacs-jupyter is also loaded, redirect its line-region eval to
     ;; cell-aware eval.  `<remap> <COMMAND>' operates at the command-symbol
     ;; level (not the key level), so this catches *whatever* key jupyter
