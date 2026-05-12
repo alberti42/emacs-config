42325f4571b6c2a8a1f0ba78f9961ea30df99385
Andrea Alberti <a.alberti82@gmail.com>
2026-05-12 18:54:13 +0200

    Added server support for opencode ACP

--- agent-shell-config.el
+++ agent-shell-config.el
@@ -9,6 +9,11 @@
   :custom
   (agent-shell-show-context-usage-indicator 'detailed)
   (agent-shell-opencode-default-model-id "openai/gpt-5.4")
+  (agent-shell-opencode-acp-command
+   ;; The --attach option relies on a custom modification
+   ;; in the branch acp-attach of opencode personal fork:
+   ;; https://github.com/alberti42/fork-opencode
+   '("opencode" "acp" "--attach" "http://localhost:4096"))
   :bind (:map agent-shell-mode-map
               ("M-RET" . newline)
               ("C-c a" . agent-shell-prompt-compose)))
