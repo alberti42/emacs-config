9d05ba46544b827bbda000a18a2a9522c4af306d
Andrea Alberti <a.alberti82@gmail.com>
2026-06-04 18:58:43 +0200

    Removed custom functions for indenting because not needed
    We can use C-x TAB and then the left/right arrow keys.

--- syntaxes.el
+++ syntaxes.el
@@ -13,26 +13,6 @@
 (setq-default tab-width 2)
 (setq-default standard-indent 2)
 
-;; Indent/unindent region by tab-width (like Sublime's option-[ / option-])
-(defun emacs-config-indent-left ()
-  "Shift selected lines (or current line) left by `tab-width' columns."
-  (interactive)
-  (let ((beg (if (use-region-p) (region-beginning) (line-beginning-position)))
-        (end (if (use-region-p) (region-end) (line-end-position))))
-    (indent-rigidly beg end (- tab-width))
-    (setq deactivate-mark nil)))
-
-(defun emacs-config-indent-right ()
-  "Shift selected lines (or current line) right by `tab-width' columns."
-  (interactive)
-  (let ((beg (if (use-region-p) (region-beginning) (line-beginning-position)))
-        (end (if (use-region-p) (region-end) (line-end-position))))
-    (indent-rigidly beg end tab-width)
-    (setq deactivate-mark nil)))
-
-(bind-key* "C-," #'emacs-config-indent-left)
-(bind-key* "C-." #'emacs-config-indent-right)
-
 ;; Load all syntax files in the syntaxes/ subdirectory 
 (let ((dir (expand-file-name "syntaxes" emacs-config-dir)))
   (when (file-directory-p dir)
