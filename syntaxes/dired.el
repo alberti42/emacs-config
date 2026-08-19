99b970c565efa6ebcd24d1221e6cf42d12bd5089
Andrea Alberti <a.alberti82@gmail.com>
2026-08-19 18:15:10 +0200

    dired: disable soft wrap mode

--- syntaxes/dired.el
+++ syntaxes/dired.el
@@ -6,7 +6,14 @@
 (when emacs-config-syntaxes-enable-dired
   (add-hook 'dired-mode-hook
             (lambda ()
-              (display-line-numbers-mode -1))))
+              (display-line-numbers-mode -1)
+              ;; No wrapping of long file lines: truncate instead.
+              (when (bound-and-true-p soft-wrap-mode)
+                (soft-wrap-mode -1))
+              (visual-line-mode -1)
+              (setq-local truncate-lines t)
+              (setq-local word-wrap nil)
+              (setq-local truncate-partial-width-windows nil))))
 
 (provide 'syntaxes-dired)
 
