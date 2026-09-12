d2de1db4f01cfaaa7c6689137da72ddb8ee30d1b
Andrea Alberti <a.alberti82@gmail.com>
2026-09-12 23:17:42 +0200

    Fixed popon package

--- warning-toast.el
+++ warning-toast.el
@@ -27,7 +27,7 @@
 (use-package popon
   :straight (popon
              :type git
-             :repo "https://codeberg.org/alberti42/emacs-popon.git"
+             :repo "https://codeberg.org/alberti42/fork-emacs-popon.git"
              :branch "fix/make-framebuffer-invisible-loop")
   :defer t)
 
